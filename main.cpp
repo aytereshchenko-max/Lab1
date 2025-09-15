809481f709a58ea549c547ceb01a597a8ecdd252
aytereshchenko-max <a.y.tereshchenko@student.khai.edu>
2025-09-15 11:00:48 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    //Задача Begin 40
+    //Декларація змінних
+    float a, b, P;
+    //введення змінних
+    cout << "Enter a:";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+    //розрахунок результату
+    P = 2 * a + b;
+    //вивід результату
+    cout << "P=" << P << endl;
+    //Задача
+    
+}
